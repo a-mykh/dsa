8e8d1088a13846d29fed006c60c4738131c85dab
Artem Mykhailov <amykhdev@gmail.com>
2025-04-07 22:52:59 +0000

    feat(LeetCode): 2874 problem description

--- LeetCode/2873-maximum-value-of-an-ordered-triplet-i/answer-1594111934.ts
+++ LeetCode/2873-maximum-value-of-an-ordered-triplet-i/answer-1594111934.ts
@@ -2,7 +2,7 @@
  * Approach: Greedy with single traversal
  * 
  * The algorithm uses the idea of iteratively updating a maximum value as it traverses the input.
- * It avoids nested loops by maintaining maxK and maxDiff.
+ * It avoids nested loops by maintaining `maxK` and `maxDiff`.
  * While the idea is similar to Kadane's algorithm 
  * (which is is specifically designed to find the maximum sum of a contiguous subarray within a one-dimensional array),
  * this solution is not finding a contiguous subarray,
@@ -10,12 +10,12 @@
  * 
  * The loop iterates through the nums array from left to right.
  * The calculations are performed in the backward order:
- * 1) maxV is 3rd part of the triplet calculation.
- * 2) maxDiff is 2nd part of the triplet calculation.
- * 3) maxK is 1st part of the triplet calculation.
+ * 1) `maxV` is 3rd part of the triplet calculation.
+ * 2) `maxDiff` is 2nd part of the triplet calculation.
+ * 3) `maxK` is 1st part of the triplet calculation.
  * Because we want to use the values from previous iteration before they are updated.
  * This approach helps traverse the input with single loop
- * and avoid additional calculation with "i" inside each iteration.
+ * and avoid additional calculation with `i` inside each iteration.
  * 
  * Time: O(n)
  * Space: O(1)
