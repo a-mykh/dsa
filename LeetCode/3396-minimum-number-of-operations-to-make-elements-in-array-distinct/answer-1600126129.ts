f9c3f44f69178e78a28577d63097c1839735516c
Artem Mykhailov <amykhdev@gmail.com>
2025-04-08 04:05:47 +0000

    feat(LeetCode): 3396 index, problem, answers

---
+++ LeetCode/3396-minimum-number-of-operations-to-make-elements-in-array-distinct/answer-1600126129.ts
@@ -0,0 +1,19 @@
+/**
+ * Approach: Hash Table, Reverse Traversal (Backward)
+ * 
+ * Time: O(n)
+ * Space: O(n)
+ */
+function minimumOperations(nums: number[]): number {
+    const hash = new Set<number>();
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (hash.has(nums[i])) {
+            return (i / 3 >> 0) + 1;
+        }
+
+        hash.add(nums[i]);
+    }
+
+    return 0;
+};
