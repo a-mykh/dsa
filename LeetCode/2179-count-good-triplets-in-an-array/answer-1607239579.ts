958aec21f897766da96feba0569bf28ac0dec046
Artem Mykhailov <amykhdev@gmail.com>
2025-04-19 22:29:05 +0000

    feat(LeetCode): 2179 answer notes

--- LeetCode/2179-count-good-triplets-in-an-array/answer-1607239579.ts
+++ LeetCode/2179-count-good-triplets-in-an-array/answer-1607239579.ts
@@ -1,9 +1,35 @@
 /**
  * Approach: Policy Based Data Structure with:
- * - Hash Map
- * - Binary search
+ * - Hash Map for fast lookups of indices in `nums2`
+ * - Binary search to find the insertion point in a sorted list of indices
+ * - Combinatorial Counting based on the relative positions of indices in sorted list 
+ *      to determine the number of good triplets
  * 
- * Time: O(n²), because of Array.splice inside of `for` loop
+ * Hash Map `nums2IndicesMap` is used to transform `nums2` array to dictionary with array item value as a key, e.g.:
+ *      [4,1,0,2,3] -> {4:0, 1:1, 0:2, 2:3, 3:4}
+ *      Note, array values are permutations array indices. This also means the value are unique.
+ *      Later the map is used for quick retrieval of the index of specific value from `nums2`.
+ * 
+ * The code iterates through each number `num` in `nums1`.
+ *      `sortedIndices` maintains a sorted list of the indices from `nums2` 
+ *          that correspond to the elements processed from `nums1`.
+ *      Current `num` is considered as a middle element of a good triplet.
+ *      `left` is a number of elements that are smaller than the current `nums2Idx`.
+ *          These represent the potential first elements of a good triplet. 
+ *          Their corresponding values in `nums1` appeared earlier and had a smaller index in `nums2`.
+ *      `right` is a number of elements in `nums2` that have a larger index than the current `nums2Idx` 
+ *          and whose corresponding values in `nums1` have not yet been encountered. 
+ *          Their indices are not yet in `sortedIndices`. 
+ *          These represent the potential third elements of a good triplet.
+ *      The product of `left` and `right` gives the number of good triplets 
+ *          that can be formed with the current element as the middle element.
+ *      `Array.prototype.slice` is used to ensure that sortedIndices remains sorted, 
+ *          which is essential for binary search.
+ * 
+ * Time: O(n²), because:
+ *      - O(n) to build nums2IndicesMap,
+ *      - O(n*log(n)) for main loop (without splice) due to Binary Search O(log(n)) performed `n` times
+ *      - O(n²) for Array.prototype.splice in main loop when `n` is large. `slice` takes O(n) by itself
  * Space: O(n)
  */
 function goodTriplets(nums1: number[], nums2: number[]): number {
